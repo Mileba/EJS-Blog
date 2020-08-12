cd9dd697a7a81b69ec7cc78a5bd1eb8fadaf3995
Mileba <mykellforbes@gmail.com>
2020-08-12 15:58:16 +0100

    wip

--- app.js
+++ app.js
@@ -12,33 +12,48 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
-app.get("/",function(req,res){
-  res.render("home",{homeContent:homeStartingContent});
+const posts = [];
+
+app.get("/", function (req, res) {
+  res.render("home", {
+    homeContent: homeStartingContent,
+    posts:posts
+  });
+
 });
 
-app.get("/about",function(req,res){
-  res.render("about",{content:aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", {
+    content: aboutContent
+  });
 });
 
-app.get("/contact",function(req,res){
-  res.render("contact",{content:contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", {
+    content: contactContent
+  });
 });
 
-app.get("/compose",function(req,res){
+app.get("/compose", function (req, res) {
   res.render("compose");
 });
 
-app.post("/compose",function(req,res){
-  const post = req.body.post;
-  console.log(post);
+app.post("/compose", function (req, res) {
+  let post = {
+    title: req.body.title,
+    post: req.body.post
+  };
+  posts.push(post);
+  res.redirect("/");
 });
 
 
 
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
